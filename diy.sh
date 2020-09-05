2763adeedb732a520a147d4fbc9ab61d1822910b
dlovew <59035223+dlovew@users.noreply.github.com>
2020-09-05 09:57:43 +0800

    Initial commit

---
+++ diy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
+git clone https://github.com/ntlf9t/luci-app-dnspod.git package/luci-app-dnspod
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/Leo-Jo/luci-app-koolproxyR.git package/luci-app-koolproxyR
